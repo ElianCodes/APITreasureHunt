864a3ed9cc7ecfc69c247a3b8b7f6f055a029e1c
Elian Van Cutsem <hello@elian.codes>
2021-05-29 15:37:57 +0200

    Index HTML

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors')
 const PORT = 8080
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 const errormsg = "You broke the game... Internal Server Error"
 
+// basic welcome page
+app.get("/", async (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html")
+})
+
 app.get("/api/getcode", async function (req, res) {
+    try {
+        res.status(200).json({ secretCode: '0216549'})
+    } catch (e) {
+      if (e) throw e
+        res.status(500).send(errormsg)
+    }
+})
+
+app.route("/api/decode").post(async function (req, res) {
+    try {
+        res.status(200).json(req.body.code)
+    } catch (e) {
+      if (e) throw e
+        res.status(500).send(errormsg)
+    }
+}).get(async function (req, res) {
+    res.status(404).json({ error: "Sure you're using the right method?"})
+}).put(async function (req, res) {
+    res.status(404).json({ error: "Sure you're using the right method?"})
+}).delete(async function (req, res) {
+    res.status(404).json({ error: "Sure you're using the right method?"})
+})
+
+app.get("/api/submit", async function (req, res) {
     try {
         res.status(200).json({ secretCode: '0216549'})
     } catch (e) {
