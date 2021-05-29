448c0397e749e9a27c494e8f20aa2cbb15dac4de
Elian Van Cutsem <hello@elian.codes>
2021-05-29 17:51:23 +0200

    verify user identity

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const PORT = 8080
+const jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +11,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
 const errormsg = "You broke the game... Internal Server Error"
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const pass = "8eGzDaarhChQmcXr"
+
+mongoose.connect(
+    "mongodb+srv://elian:8eGzDaarhChQmcXr@trasurehunts.1emy8.mongodb.net/TreasureHunt?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+);
+
+const User = new Schema({
+  name:  String, // String is shorthand for {type: String}
+  step: Number,
+  completedAt: Date
+});
+const users = mongoose.model("users", User)
 
 // basic welcome page
 app.get("/", async (req, res) => {
@@ -87,9 +109,43 @@ app.get("/api/deadcount/walter/:number", async function (req, res) {
     }
 })
 
+app.post('/api/login', async (req, res) => {
+    try {
+        if(req.body.name == null){
+            res.status(400).json({ msg: "it looks like no name was attached"})
+        } else if (req.body.name.trim().length == 0) {
+            res.status(400).json({ msg: "You tried to trick me right"})
+        } else {
+            const user = await users.findOne({ name: req.body.name }).exec();   
+            if(user != null) {
+                const token = jwt.sign({ name: user.name }, 'smollPP')
+                res.status(200).json({ msg: "Username already exists", token: token})
+            } else {
+                const newUser = await users.create({name: req.body.name, step: 4, completedAt: Date.now()})
+                const token = jwt.sign({ name: newUser.name }, 'smollPP')
+                res.status(201).json(token)
+            }
+        }
+    } catch (e) {
+        if (e) throw e
+        res.status(500).send(errormsg)
+    }
+})
+
 app.get("/api/submit", async function (req, res) {
     try {
-        res.status(200).json({ secretCode: '0216549'})
+        const auth = req.headers.authorization;
+        if (auth == null) {
+            return res.status(401).json({ msg: "No token, auth denied" });
+          }
+        const bearer = auth.split(' ');
+        const token = bearer[1];
+        if (!token) {
+          return res.status(401).json({ msg: "No token, auth denied" });
+        }
+        const decoded = jwt.verify(token, "smollPP");
+        req.user = decoded.user;
+        res.status(200).json({ msg: 'Congrats! You have completed the treasure hunt!'})
     } catch (e) {
       if (e) throw e
         res.status(500).send(errormsg)
